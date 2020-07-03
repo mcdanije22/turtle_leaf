c87d9ecafaea4f8bf34ef176eb526cc4bbea6207
mcdanije22 <mcdanije22@gmail.com>
2020-07-03 16:31:52 -0400

    updating news page

--- src/pages/news.js
+++ src/pages/news.js
@@ -2,11 +2,12 @@ import React from "react"
 import "./styles/news.scss"
 import Layout from "../components/layout"
 import { useStaticQuery, graphql } from "gatsby"
-import { Button, Row, Col } from "antd"
+import { Button, Row, Col, Card } from "antd"
 import Img from "gatsby-image"
 import { Link } from "gatsby"
 
 const News = () => {
+  const { Meta } = Card
   const data = useStaticQuery(graphql`
     query {
       landing_photo: file(relativePath: { eq: "landing_photo2.jpg" }) {
@@ -60,30 +61,39 @@ const News = () => {
         >
           <h1>What's New</h1>
         </div>
-        {data.allMarkdownRemark.edges.map((post, i) => {
-          return (
-            <div className="newsStory" key={i}>
-              {/* <div className="newsLeftSide">
-                  <Img
-                    fluid={
-                      post.node.frontmatter
-                        .featuredImage.childImageSharp.fluid
+        <Row justify="center">
+          {data.allMarkdownRemark.edges.map((post, i) => {
+            return (
+              <Col key={i}>
+                <div className="newsStory">
+                  <Card
+                    style={{ width: 300 }}
+                    cover={
+                      <Img
+                        fluid={
+                          post.node.frontmatter.featuredImage.childImageSharp
+                            .fluid
+                        }
+                      />
                     }
-                  />
-                </div> */}
-              {/* <div className="newsRightSide"> */}
-              <h2>{post.node.frontmatter.date}</h2>
-              <h3>{post.node.frontmatter.title}</h3>
-              <p>{post.node.excerpt}</p>
-              <Link to={post.node.frontmatter.slug}>
-                <Button type="link" style={{ padding: "0" }}>
-                  Read More...
-                </Button>
-              </Link>
-              {/* </div> */}
-            </div>
-          )
-        })}
+                    actions={[]}
+                  >
+                    <div className="newsRightSide">
+                      <p>{post.node.frontmatter.date}</p>
+                      <h3>{post.node.frontmatter.title}</h3>
+                      <p>{post.node.excerpt}</p>
+                      <Link to={post.node.frontmatter.slug}>
+                        <Button type="link" style={{ padding: "0" }}>
+                          Read More...
+                        </Button>
+                      </Link>
+                    </div>
+                  </Card>
+                </div>
+              </Col>
+            )
+          })}
+        </Row>
       </div>
     </Layout>
   )
