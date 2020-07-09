8c1cfd3b38f1e4bc0af08258dbd07890ceee6077
mcdanije22 <mcdanije22@gmail.com>
2020-07-09 18:12:31 -0400

    lightbox added, modded to work with project

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,11 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import "./styles/gallery.scss"
 import Layout from "../components/layout"
 import { useStaticQuery, graphql } from "gatsby"
 import { Row, Col } from "antd"
 import Img from "gatsby-image"
+import Lightbox from "react-image-lightbox"
+import "react-image-lightbox/style.css"
 
 const Gallery = () => {
+  const [photoIndex, setIndex] = useState(0)
+  const [isOpen, setOpenStatus] = useState(false)
   const data = useStaticQuery(graphql`
     query {
       landing_photo: file(relativePath: { eq: "landing_photo2.jpg" }) {
@@ -15,17 +19,10 @@ const Gallery = () => {
           }
         }
       }
-      adamHeadShot: file(relativePath: { eq: "adam_headshot.jpg" }) {
-        childImageSharp {
-          fluid(quality: 90, maxWidth: 400) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
       allImageSharp {
         edges {
           node {
-            fluid(quality: 90, maxWidth: 400) {
+            fluid(quality: 90, maxWidth: 1920) {
               ...GatsbyImageSharpFluid
             }
           }
@@ -34,6 +31,7 @@ const Gallery = () => {
     }
   `)
   console.log(data)
+  console.log(data.allImageSharp.edges.length)
   return (
     <Layout>
       <div id="galleryContainer">
@@ -49,16 +47,46 @@ const Gallery = () => {
           <h1>Gallery</h1>
         </div>
         <div id="galleryPhotos">
-          <Row justify="center" gutter={[24, 20]}>
+          <Row gutter={[24, 20]}>
             {data.allImageSharp.edges.map((image, i) => {
               return (
-                <Col xs={24} sm={8} key={i}>
+                <Col
+                  xs={24}
+                  sm={8}
+                  key={i}
+                  onClick={() => {
+                    setIndex(i)
+                    setOpenStatus(true)
+                  }}
+                >
                   <Img fluid={image.node.fluid} />
                 </Col>
               )
             })}
           </Row>
         </div>
+        {isOpen && (
+          <Lightbox
+            mainSrc={data.allImageSharp.edges[photoIndex].node.fluid.src}
+            nextSrc={data.allImageSharp.edges[photoIndex].node.fluid.src}
+            prevSrc={data.allImageSharp.edges[photoIndex].node.fluid.src}
+            onCloseRequest={() => setOpenStatus(false)}
+            onMovePrevRequest={() =>
+              setIndex(
+                photoIndex === 0
+                  ? data.allImageSharp.edges.length - 1
+                  : photoIndex - 1
+              )
+            }
+            onMoveNextRequest={() =>
+              setIndex(
+                photoIndex + 1 > data.allImageSharp.edges.length - 1
+                  ? 0
+                  : photoIndex + 1
+              )
+            }
+          />
+        )}
       </div>
     </Layout>
   )
